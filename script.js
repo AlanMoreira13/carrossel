b85c3984ebc6b8a3f6ca567a808db9c20de74698
Alan Moreira <moreiraalan22@gmail.com>
2022-03-06 09:39:31 -0300

    Alterações na resposinvidade

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 'use strict'
+'use strict'
 
 const images = [
     {"id": "1", "url": "./assets/itachi4.jpg"},
@@ -13,56 +13,9 @@ const images = [
     {"id": "11", "url": "./assets/naruto.jpg"},
     {"id": "12", "url": "./assets/kakashi2.jpg"},
     {"id": "13", "url": "./assets/naruto2.jpeg"},
-
 ]
 
-// const containerItems = document.querySelector("#container-items"); 
 
-// const loadImages = (images, container) => {
-//     images.forEach (image => {
-//         container.innerHTML += `
-//         <div classe="item">
-//         <img src="${image.url}">
-//         </div> 
-//         `
-//     });
-// }
-
-// loadImages(images, containerItems);
-
-// let items = document.querySelectorAll(".item");
-
-// const previous = () => {
-//     containerItems.appendChild(items[0]);
-//     items = document.querySelectorAll(".item");
-// }
-
-// const next = () => {
-//     const lastItem = items[items.length - 1];
-//     containerItems.insertBefore(lastItem, items[0])
-//     items = document.querySelectorAll(".item")
-// }
-
-
-// document.querySelector("#previous").addEventListener("click", previous);
-// document.querySelector("#next").addEventListener("click", next);
-
-
-
-
-
-
-
-'use strict';
-
-// const images = [
-//     { 'id': '1', 'url':'./img/chrono.jpg' },
-//     { 'id': '2', 'url':'./img/inuyasha.jpg' },
-//     { 'id': '3', 'url':'./img/tenchi.jpg' },
-//     { 'id': '4', 'url':'./img/tenjhotenge.jpg' },
-//     { 'id': '5', 'url':'./img/yuyuhakusho.jpg' },
-//     { 'id': '6', 'url':'./img/ippo.png' },
-// ]
 
 const containerItems = document.querySelector('#container-items');
 
